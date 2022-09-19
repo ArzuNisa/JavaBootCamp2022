7d72aba4245e2fe0acc2e687609d7a1025b03c72
ArzuNisa <arzunisaylcnky@gmail.com>
2022-09-19 19:43:59 +0300

    variables

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -2,7 +2,15 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!!");
+		//Variable names are defined as CamelCase
+		System.out.println("Öğrenci sayım : 12");
+		
+		int ogrenciSayisi = 12;
+		String mesaj ="Öğrenci sayım : ";
+		
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println("Öğrenci sayım : "+ogrenciSayisi);
+		
 	}
 
 }
