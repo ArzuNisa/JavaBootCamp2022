0d3aca227337effdca81114162f44eca32cb2a34
ArzuNisa <arzunisaylcnky@gmail.com>
2022-09-19 19:57:33 +0300

    test

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -2,15 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		//Variable names are defined as CamelCase
-		System.out.println("Öğrenci sayım : 12");
-		
-		int ogrenciSayisi = 12;
-		String mesaj ="Öğrenci sayım : ";
-		
-		System.out.println(mesaj + ogrenciSayisi);
-		System.out.println("Öğrenci sayım : "+ogrenciSayisi);
-		
+		System.out.println("Hello World!!");
 	}
 
 }
