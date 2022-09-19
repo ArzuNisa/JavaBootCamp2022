eccb6573fd3c579bdf6f4fa2d968742c615a4c9d
ArzuNisa <arzunisaylcnky@gmail.com>
2022-09-19 19:53:47 +0300

    testing

---
+++ Variables/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	//Variable names are defined as CamelCase
+		System.out.println("Öğrenci sayım : 12");
+				
+		int ogrenciSayisi = 12;
+		String mesaj ="Öğrenci sayım : ";
+				
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println("Öğrenci sayım : "+ogrenciSayisi);
+				
+	}
+
+}
