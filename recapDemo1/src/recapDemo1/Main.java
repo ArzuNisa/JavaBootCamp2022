a8e20164a7ff1d54e0b3fb28590792221fe22ced
ArzuNisa <arzunisaylcnky@gmail.com>
2022-09-19 22:52:19 +0300

    Finding the largest number

---
+++ recapDemo1/src/recapDemo1/Main.java
@@ -0,0 +1,18 @@
+package recapDemo1;
+
+public class Main {
+
+	public static void main(String[] args) {		// TODO Auto-generated method stub
+		int sayi1 =20, sayi2 =25, sayi3 =2;
+		int enBuyuk = sayi1;
+		
+		if(enBuyuk<sayi2) {
+			enBuyuk = sayi2;
+		}
+		if(enBuyuk<sayi3){
+			enBuyuk=sayi3;
+		}
+		System.out.println("En büyük = "+enBuyuk);
+	}
+
+}
